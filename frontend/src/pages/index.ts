b8cbe68316a7a235e77a6186bca39c3256c0c28d
Mattia <mattialemma2017@gmail.com>
2026-03-10 15:58:28 +0100

    (add) - Log out error | Notion fix

---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,7 @@
+export { LoginScreen as LoginPage } from "../features/auth/LoginScreen";
+export { RetrieveStep1Screen as RecoverPasswordRequestPage } from "../features/auth/RetrieveStep1Screen";
+export { RetrieveStep2Screen as RecoverPasswordVerifyPage } from "../features/auth/RetrieveStep2Screen";
+export { ProjectsScreen as ProjectsPage } from "../features/projects/ProjectsScreen";
+export { ProjectIssuesScreen as ProjectIssuesPage } from "../features/projects/ProjectIssuesScreen";
+export { IssuePage } from "../features/issue/IssuePage";
+export { ManageAccountSettingsPage } from "../features/settings/ManageAccountSettingsPage";
