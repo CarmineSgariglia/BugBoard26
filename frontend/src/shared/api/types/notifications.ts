76876cb147a4e2dd9a2c7c6502ca70537da1a1c9
Mattia <mattialemma2017@gmail.com>
2026-03-11 18:59:15 +0100

    (add) - Notification logic pages

--- frontend/src/shared/api/types/notifications.ts
+++ frontend/src/shared/api/types/notifications.ts
@@ -1,7 +1,18 @@
-﻿export type NotificationItem = {
+export type NotificationType =
+  | "PROJECT_ADDED"
+  | "UNASSIGNED_PROJECT"
+  | "PROJECT_REMOVED"
+  | "ISSUE_ASSIGNED"
+  | "ISSUE_ADDED"
+  | "ISSUE_CLOSED"
+  | "ISSUE_UNASSIGNED"
+  | "ISSUE_UPDATED"
+  | (string & {}); // fallback for future types
+
+export type NotificationItem = {
   notifyUserId: number;
   notificationId: number;
-  type: string;
+  type: NotificationType;
   createdAt: string;
   issueId?: number | null;
   projectId?: number | null;
