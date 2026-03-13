88bac06f3635ca6a233afc1e869c0dce62eda626
Massimo127r <maxrusso127@gmail.com>
2026-03-13 16:35:35 +0100

    fix(codesmell): permission not used in any part of the code

--- backend/apps/bugboardapi/permissions/__init__.py
+++ backend/apps/bugboardapi/permissions/__init__.py
@@ -1,10 +1,10 @@
-from .base import IsAdminUser, is_admin
+from .base import is_admin
 from .issues import IsAssigneeOrAdmin, check_assignee_or_admin, ensure_issue_access
 from .projects import IsProjectMember, check_admin, ensure_project_access, user_project_ids
 
 
 __all__ = [
-    "IsAdminUser",
+
     "IsAssigneeOrAdmin",
     "IsProjectMember",
     "check_assignee_or_admin",
