0785c8a55aa3a81c3f8a09aee0ceb5ded2ecf09b
Mattia Lemma <mattialemma@MacBook-Pro-di-Mattia.local>
2026-03-23 17:42:12 +0100

    (fix) - email

--- frontend/src/shared/lib/help.ts
+++ frontend/src/shared/lib/help.ts
@@ -3,5 +3,5 @@
  * with the support email address pre-filled.
  */
 export const handleGetHelp = () => {
-    window.location.href = "mailto:supporto@bugboard.it";
+    window.location.href = "mailto:support@bugboard.it";
 };
